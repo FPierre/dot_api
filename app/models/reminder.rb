94ae50288c39d9441b84b611753aa75d2a64340b
FPierre <pflauder@gmail.com>
2016-04-28 09:27:07 +0200

    User has_many Reminders et Reminder belongs_to User

--- app/models/reminder.rb
+++ app/models/reminder.rb
@@ -1,4 +1,6 @@
 class Reminder < ApplicationRecord
+  belongs_to :user
+
   validates :title, :content, presence: true
 
   after_create -> { ActionCable.server.broadcast 'reminder_channel', { reminder: self }}
