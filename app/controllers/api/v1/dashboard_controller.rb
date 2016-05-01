18e68d4ab928a0051c147bc9fe67c531caa22c3a
FPierre <pflauder@gmail.com>
2016-05-01 17:34:40 +0200

    Action de demande de chemin depuis SARAH

--- app/controllers/api/v1/dashboard_controller.rb
+++ app/controllers/api/v1/dashboard_controller.rb
@@ -2,34 +2,44 @@ module Api
   module V1
     class DashboardController < ApplicationController
       api :GET, '/dashboard/path/from/:from/to/:to', 'Display the path between two cities'
-      description 'Request Google API to get the car path between the city "from" and the other city "to"'
+      description 'Make a request to the Google Map API to search the car route between the city "from" and the city "to".'
       error code: 400, desc: 'Bad request'
-      meta clients: [:sarah], status: :pending
+      error code: 200, desc: 'Ok'
+      meta clients: [:sarah], status: :ok
       param :from, String, desc: 'Departure city', required: true
-      param :to, String, desc: 'Arrival city', required: true
+      param :to,   String, desc: 'Arrival city',   required: true
       def path
         if params[:from].blank? || params[:to].blank?
-          render json: { status: :bad_request, message: "Absence du paramètre from ou to" } and return
+          render json: { message: "Absence du paramètre from ou to" }, status: :bad_request and return
         end
 
         request_from = Geocoder.search params[:from]
         request_to = Geocoder.search params[:to]
 
         if request_from.blank? || request_to.blank?
-          render json: { status: :bad_request, message: "Erreur lors de la recherche de l'itinéraire" } and return
+          render json: { message: "Erreur lors de la recherche de l'itinéraire" }, status: :bad_request and return
         end
 
-        from = request_from&.first&.data['geometry']['location'],
+        # TODO Idiom pour gérer le code suivant plus proprement ?
+
+        from = request_from&.first&.data['geometry']['location']
         to = request_to&.first&.data['geometry']['location']
 
-        path = {
-          from: { lat: from.dig('lat'), lon: from.dig('lon') },
-          to: { lat: to.dig('lat'), lon: to.dig('lon') }
-        }
+        if from.kind_of? Array
+          from = { lat: from[0].dig('lat'), lon: from[0].dig('lng') }
+        else
+          from = { lat: from.dig('lat'), lon: from.dig('lng') }
+        end
 
-        ActionCable.server.broadcast 'path_channel', path: path
+        if to.kind_of? Array
+          to = { lat: to[0].dig('lat'), lon: to[0].dig('lng') }
+        else
+          to = { lat: to.dig('lat'), lon: to.dig('lng') }
+        end
 
-        render head :ok and return
+        ActionCable.server.broadcast 'path_channel', path: { from: from, to: to }
+
+        head :ok
       end
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,23 +6,25 @@ Rails.application.routes.draw do
       get 'ping', to: 'ping#ping', as: :ping
 
       devise_scope :user do
-        # post 'users/sign_in', to: 'users/sessions#create', as: :user_session
+        post 'sign_in', to: 'users/sessions#create', as: :user_session
         # delete 'users/sign_out', to: 'users/sessions#destroy', as: :destroy_user_session
+        delete 'users', to: 'users/registrations#destroy', as: :destroy_user_registration
         post 'users', to: 'users/registrations#create', as: :user_registration
         # put 'users', to: 'users/registrations#update'
       end
 
-      # scope 'dashboard', controller: :dashboard do
-        # get 'path/from/:from/to/:to', action: :path
-      # end
+      scope 'dashboard', controller: :dashboard do
+        get 'path/from/:from/to/:to', action: :path
+      end
 
-      # scope 'settings', controller: :settings do
+      scope 'settings', controller: :settings do
       #   post 'reminders-state', action: :reminders_state, as: :reminders_state
       #   post 'sarah-state', action: :sarah_state, as: :sarah_state
       #   post 'twitter-state', action: :twitter_state, as: :twitter_state
       #   post 'weather-state', action: :weather_state, as: :weather_state
       #   post 'metting-room-state', action: :metting_room_state, as: :metting_room_state
-      # end
+        get 'sarah-commands', action: :sarah_commands, as: :sarah_commands
+      end
 
       resources :reminders, only: [:create]
     end
