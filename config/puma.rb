8e30304a8afc1ddc6aee81f0ab2ee7612d2efda8
FPierre <pflauder@gmail.com>
2016-06-09 20:43:01 +0200

    Configuration Puma

--- config/puma.rb
+++ config/puma.rb
@@ -43,7 +43,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 #   ActiveRecord::Base.establish_connection if defined?(ActiveRecord)
 # end
 
-# bind 'unix:///var/www/dot_api/tmp/sockets/puma.sock'
+bind 'unix:///var/www/dot_api/tmp/sockets/puma.sock'
 pidfile 'tmp/pids/puma.pid'
 state_path 'tmp/pids/puma.state'
 stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
