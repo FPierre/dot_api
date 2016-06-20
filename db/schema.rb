838add6da727e2424792f82b2c7aeef3b259c5f2
FPierre <pflauder@gmail.com>
2016-06-20 22:51:29 +0200

    Schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -43,11 +43,6 @@ ActiveRecord::Schema.define(version: 20160620204731) do
     t.boolean  "screen_guest_enabled", default: false
   end
 
-  create_table "tests", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                default: "",    null: false
     t.string   "encrypted_password",   default: "",    null: false
