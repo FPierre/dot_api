fabb6f6992cef1660d8ce54b914dcf544b79ac75
FPierre <pflauder@gmail.com>
2016-07-06 22:10:29 +0200

    Mise à jour du Serializer de Raspberry

--- app/serializers/raspberry_serializer.rb
+++ app/serializers/raspberry_serializer.rb
@@ -1,3 +1,3 @@
 class RaspberrySerializer < ActiveModel::Serializer
-  attributes :id, :api_port, :created_at, :ip_address, :mac_address, :master_device, :name
+  attributes :id, :api_port, :ip_address, :mac_address, :master_device, :name
 end
