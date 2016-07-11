39bb1d4c6d5e9f26245e29ba12af89e79491cc65
FPierre <pflauder@gmail.com>
2016-07-11 16:16:57 +0200

    Attaque des Raspberry par le nom de domaine plutot que l'IP

--- lib/raspberry_api_connector.rb
+++ lib/raspberry_api_connector.rb
@@ -13,13 +13,16 @@ class RaspberryApiConnector
     raspberry = Raspberry.find_by master_device: true
 
     @api_port = raspberry.api_port
-    @api_url  = raspberry.ip_address
+    @api_url  = raspberry.domain_name
+
+    ap @api_port
+    ap @api_url
   rescue NoMethodError => e
     raise Error.new "Please add 'api_url' and 'api_port' keys into your configuration"
   end
 
   def get_room_occupied options = {}
-    process { resource.get(route_for('setLedMode.py', options), headers) }
+    process { resource.get(route_for('setLedMode.py', options)) }
   end
 
   def process
