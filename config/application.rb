932afb811485745195c73a50e79b368d1b2de13d
FPierre <pflauder@gmail.com>
2016-07-11 10:55:44 +0200

    Rétablissement du Eager Load pour les libs et services

--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,9 @@ module DotApi
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    # config.autoload_paths += Dir[Rails.root.join('app', 'services', '{**/*/}')]
-    # config.autoload_paths += Dir[Rails.root.join('lib')]
-
-    # config.eager_load_paths += Dir[Rails.root.join('lib')]
+    config.autoload_paths += Dir[Rails.root.join('app', 'services', '{**/*/}')]
+    config.autoload_paths += Dir[Rails.root.join('lib')]
+    config.eager_load_paths += Dir[Rails.root.join('lib')]
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
